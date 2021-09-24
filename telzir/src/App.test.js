a13985c81fd2eeb59dd6ef8f9866858002c07de6
Uilton <itoclick@gmail.com>
2021-09-23 23:50:29 -0300

    subindo testes

--- telzir/src/App.test.js
+++ telzir/src/App.test.js
@@ -1,8 +1,84 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('Renderiza o texto: Calcule o Valor do Seu Plano', () => {
-  render(<App />);
-  const textElement = screen.getByText(/Calcule o Valor do Seu Plano/i);
-  expect(textElement).toBeInTheDocument();
+
+describe('1 - Testando Componentes Renderizados', () => {
+  test('1-1 Renderiza o texto: Calcule o Valor do Seu Plano', () => {
+    render(<App />);
+    const textElement = screen.getByText(/Calcule o Valor do Seu Plano/i);
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test('1-2 Renderiza os campos: Plano - Origem - Destino - Minutos', () => {
+    render(<App />);
+    const listElement = [];
+    listElement.push(screen.getByDisplayValue('--Selecione um Plano--'));
+    listElement.push(screen.getByDisplayValue('--De Onde--'));
+    listElement.push(screen.getByDisplayValue('--Para Onde--'));
+    listElement.push(screen.getByDisplayValue('0'));
+    
+    expect(listElement[3].nodeName).toEqual("INPUT");    
+    expect(listElement.length).toEqual(4);
+  });
+
+  test('1-3 Renderiza os campos: Preço com plano e Preço sem plano', () => {
+    render(<App />);
+    const listElement = [];
+    listElement.push(screen.getByText('Com o Fale mais R$ 0.00'));
+    listElement.push(screen.getByText('Sem o Fale Mais R$ 0.00'));
+    
+    expect(listElement.length).toEqual(2);
+    expect(listElement[0].nodeName).toEqual("H1");
+  });
 });
+
+describe('2 - Testando Componentes e Calculo do Preço', () => {
+
+  test('2-1 Renderiza um Botão com texto: Calculr', () => {
+    render(<App />);
+    const Element = screen.getByText(/Calcular/i);
+    
+    expect(Element).toBeInTheDocument();
+    expect(Element.tagName).toEqual("BUTTON")
+  });
+
+  test('2-2 Renderiza um Botão com texto: Calculr', () => {
+    render(<App />);
+    const Element = screen.getByText(/Calcular/i);
+    
+    expect(Element).toBeInTheDocument();
+    expect(Element.tagName).toEqual("BUTTON")
+  });
+
+  test('2-3 Testando o Calcuo', () => {
+    const data = { 
+      chPlan:  '',
+      minuts:  '',
+      tax: '',
+      setwithPlan: (param) => {data.withPlan = param},
+      withPlan: '0',
+      setnoPlan: (param) => {data.noPlan = param},
+      noPlan:'0'
+    };  
+    const MockContext = React.createContext();
+    
+    render(
+      <MockContext.Provider value={ data } >
+        <App />
+      </MockContext.Provider>
+    );
+
+    const btnCalcular = screen.getByText(/Calcular/i);
+    fireEvent.click(btnCalcular);
+    
+    const comPlano = screen.getByText("Com o Fale mais R$ 0.00");
+    const semPlano = screen.getByText("Sem o Fale Mais R$ 0.00");
+    
+    expect(comPlano).toBeInTheDocument();
+    expect(semPlano).toBeInTheDocument();
+  });
+
+  
+
+})
--- telzir/src/Components/Fildes.jsx
+++ telzir/src/Components/Fildes.jsx
@@ -21,74 +21,74 @@ export default function Filds() {
     const numchPlan = Number(chPlan);
     const numminuts = Number(minuts);
     const numtax = Number(tax);
-    
-    if(numminuts <= numchPlan) { 
+
+    if (numminuts <= numchPlan) {
       setwithPlan(0);
       setnoPlan((numminuts * numtax));
     }
-    else if(numminuts > numchPlan) {
+    else if (numminuts > numchPlan) {
       setwithPlan((numminuts - numchPlan) * numtax + (numtax * 0.1));
       setnoPlan(numminuts * numtax)
     }
   };
 
 
-    return (
-      <main className="main">
-        <h1 id="topo">Calcule o Valor do Seu Plano</h1>
-        <form className="calcarea">
-          <div className="internal_form" >
+  return (
+    <main className="main">
+      <h1 id="topo">Calcule o Valor do Seu Plano</h1>
+      <form className="calcarea">
+        <div className="internal_form" >
 
-            <div>
-              <label>Escolha um plano:</label>
-              <select name="plans" id="plans" onChange={(e) => setchPlan(e.target.value)}>
-                <option value=""> --Selecione um Plano-- </option>
-                {
-                  Object.entries(data.plans).map(
-                    (e) => <option value={e[1]} key={e[1]}>{`Fale mais ${e[1]}`}</option>
-                  )
-                }
-              </select>
-            </div>
-            <div>
-
-              <label>Origem:</label>
-              <select name="origin" id="origin" onChange={(e) => setPosibleDestin(data.ddds[e.target.value])}>
-                <option value=""> --De Onde-- </option>
-                {Object.keys(data.ddds).map(
-                  (code, i) => <option value={code} key={i}> {`DDD: ${code} `}</option>
+          <div>
+            <label>Escolha um plano:</label>
+            <select name="plans" id="plans" onChange={(e) => setchPlan(e.target.value)}>
+              <option value=""> --Selecione um Plano-- </option>
+              {
+                Object.entries(data.plans).map(
+                  (e) => <option value={e[1]} key={e[1]}>{`Fale mais ${e[1]}`}</option>
                 )
-                }
-              </select>
-            </div>
-            <div>
-              <label>Destino:</label>
-              <select name="destin" id="destin" onChange={(e) => setTax(e.target.value)} >
-                <option value=""> --Para Onde-- </option>
-                {Object.entries(posibleDestin).map(
-                  (destin, i) => <option value={destin[1]} key={i}>{destin[0]}</option>
-                )
-                }
-              </select>
-            </div>
-            <div>
-              <label>Minutos:</label>
-              <input name="minuts" value={minuts} placeholder="--Minutos de Ligação--" type="number" onChange={
-                (e) => e.target.value < 0 ? setMinuts(0) : setMinuts(e.target.value)              
               }
-              />
-            </div>
-          </ div>
-          <button type="button" onClick={clickCalc} id="btncalc">Calcular</button>
-          <div className="valores">
-            <div className="plan">
-              <h1>{`Com o Fale mais  R$ ${parseFloat(withPlan).toFixed(2)}`}</h1>
-            </div>
-            <div className="noplan"><h1>{`Sem o Fale Mais  R$ ${parseFloat(noPlan).toFixed(2)}`}</h1></div>
+            </select>
           </div>
+          <div>
 
-        </form>
-      </main>
+            <label>Origem:</label>
+            <select name="origin" id="origin" onChange={(e) => setPosibleDestin(data.ddds[e.target.value])}>
+              <option value=""> --De Onde-- </option>
+              {Object.keys(data.ddds).map(
+                (code, i) => <option value={code} key={i}> {`DDD: ${code} `}</option>
+              )
+              }
+            </select>
+          </div>
+          <div>
+            <label>Destino:</label>
+            <select name="destin" id="destin" onChange={(e) => setTax(e.target.value)} >
+              <option value=""> --Para Onde-- </option>
+              {Object.entries(posibleDestin).map(
+                (destin, i) => <option value={destin[1]} key={i}>{destin[0]}</option>
+              )
+              }
+            </select>
+          </div>
+          <div>
+            <label>Minutos:</label>
+            <input name="minuts" value={minuts} placeholder="--Minutos de Ligação--" type="number" onChange={
+              (e) => e.target.value < 0 ? setMinuts(0) : setMinuts(e.target.value)
+            }
+            />
+          </div>
+        </ div>
+        <button type="button" onClick={clickCalc} id="btncalc">Calcular</button>
+        <div className="valores">
+          <div className="plan">
+            <h1>{`Com o Fale mais  R$ ${parseFloat(withPlan).toFixed(2)}`}</h1>
+          </div>
+          <div className="noplan"><h1>{`Sem o Fale Mais  R$ ${parseFloat(noPlan).toFixed(2)}`}</h1></div>
+        </div>
 
-    )
+      </form>
+    </main>
+
+  )
 }
